f40ba04dd882814f8a1d5fbdc77bc425f6c20d91
zyx45889 <52809492+zyx45889@users.noreply.github.com>
2024-09-14 15:15:35 +0900

    Update raysample_kdtree_subprocess.py

--- gen_ray_trace_info/raysample_kdtree_subprocess.py
+++ gen_ray_trace_info/raysample_kdtree_subprocess.py
@@ -1,6 +1,7 @@
 import subprocess
 import numpy as np
 import datetime
+# given cpu num, do raysample in small blocks in dynamic parallelism 
 
 cam_idx_gl=[-1,0,1,2]
 for cam in cam_idx_gl:
@@ -18,12 +19,14 @@ for cam in cam_idx_gl:
 
     cpunum=30
 
+    # build ANN tree
     cmd = 'python ./gen_ray_trace_info/raysample_kdtree.py --w1 0 --w2 0 --h1 0 --h2 0 --cam '+str(cam)+" --bt 1"
     p = subprocess.Popen(cmd, shell=True)
     while(1):
         if(p.poll() is not None):
             break
 
+    # start raysample
     cpustatues=np.zeros(cpunum)
     plist=[]
     p_cpuid=[]
@@ -31,6 +34,7 @@ for cam in cam_idx_gl:
 
     for i in range(batch1):
         for j in range(batch2):
+            # wait until some process finish
             while(1):
                 if cpustatues.sum()<cpunum:
                     break
@@ -39,6 +43,7 @@ for cam in cam_idx_gl:
                         cpustatues[p_cpuid[k]]=0
                         p_cpuid[k]=-1
             for k in range(cpunum):
+                # start a new process
                 if(cpustatues[k]==0):
                     w1=i*basew
                     w2=w1+basew
@@ -53,6 +58,7 @@ for cam in cam_idx_gl:
                     print("start on:",k)
                     break
 
+    # wait until all the processes finish
     while(cpustatues.sum()!=0):
         if cpustatues.sum()<cpunum:
                     break
